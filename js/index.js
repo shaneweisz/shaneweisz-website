90d6fbda1f3dd6ee2b26f03ce04eb28c6eccb331
Shane Weisz <shaneweisz@gmail.com>
2019-11-08 14:52:13 +0200

    Fixed bug in sound for im batman

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Colour Changing Name:
+// Colour Changing of Shane Weisz Title:
 
 var header = document.querySelector("h1");
 var img = document.getElementById("main_image");
@@ -18,34 +18,24 @@ function changeColor()
 var interval;
 interval = setInterval("changeColor()", 500);
 
-// Batman Text Changing:
+// Batman Swapping:
 
-// firstTime = true;
 function changeText()
 {
     header.textContent = "or Batman...";
     img.src = "media/batman.jpg";
     header.style.color = "black";
     clearInterval(interval);
-    var audio = new Audio('im-batman.mp3');
+    var audio = new Audio('media/im-batman.mp3');
     audio.play();
-    // if (!firstTime)
-    // {
-    //     firstTime = true;;
-    // }
 }
+
 function changeTextBack()
 {
     interval = setInterval("changeColor()", 500);
     header.textContent = "Shane Weisz";
     img.src = "https://sandtonchronicle.co.za/wp-content/uploads/sites/33/2016/12/shane-st-davids.jpg";
-    // if (count == 1)
-    // {
-    //     var popup = document.getElementById("myPopup");
-    //     popup.classList.toggle("show");
-    //     alert("I'm not saying I'm Batman, I'm just saying have you ever seen me and Batman on this website at the same time?");
-    //     count += 1;
-    // }
 }
+
 header.addEventListener("mouseover", changeText)
 header.addEventListener("mouseout", changeTextBack)
