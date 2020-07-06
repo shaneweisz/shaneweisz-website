597a830b3caf40c93f8ec831e3750f728b6e8a59
Shane Weisz <shaneweisz@gmail.com>
2020-07-06 10:50:54 +0200

    1) Renamed Test Friendship to Personal Quiz 2) Removed colour changing name 3) Added Documents and Links to NavBar

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 // Colour Changing of Shane Weisz Title:
 //
-var header = document.querySelector("h1");
-// var img = document.getElementById("main_image");
+// var header = document.querySelector("h1");
+// // var img = document.getElementById("main_image");
 
-function changeColor()
-{
-    var options = "0123456789ABCDEF";
-    var colour = "#";
-    for(var i = 1; i <= 6; i++)
-    {
-        var num = Math.floor(Math.random()*16);
-        colour += options[num];
-    }
-    header.style.color = colour;
-}
+// function changeColor()
+// {
+//     var options = "0123456789ABCDEF";
+//     var colour = "#";
+//     for(var i = 1; i <= 6; i++)
+//     {
+//         var num = Math.floor(Math.random()*16);
+//         colour += options[num];
+//     }
+//     header.style.color = colour;
+// }
 
-var interval;
-interval = setInterval("changeColor()", 500);
+// var interval;
+// interval = setInterval("changeColor()", 500);
 
 // Batman Swapping:
 
